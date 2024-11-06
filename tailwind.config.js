0c2cf5105f93c40a53bd766158df64c288352600
Andreewenas <andreewenas@gmail.com>
2024-11-06 23:57:31 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,html}"
+  ],
+  theme: {
+    fontFamily: {
+      inter: ["Inter", "sans-serif"],
+      rubik: ["Rubik Mono One", "monospace"],
+      teko: ["Teko", "sans-serif"],
+      poppins: ["Poppins", "sans-serif"],
+      fugaz: ["Fugaz One", "sans-serif"],
+    },
+  },
+  plugins: [],
+}
+
